9a6840ff9cee0336f2cb448e4c6184772ab5308a
joaov <joaov@PEREIRA>
2025-01-08 18:13:38 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
+{
+    public class Produto
+    {
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Nome { get; set; }
+
+        [Range(0,999999, ErrorMessage ="Informe um valor adequado")]
+        public int Quantidade { get; set; }
+
+        [Range(0.1,999999, ErrorMessage ="Informe um preço adequado")]
+        public double Preco { get; set; }
+
+        public double ValorEstoqueTotal()
+        {
+             return Quantidade;
+        }
+    }
+}
